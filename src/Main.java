92955faa07eddefdade4bc03fbd19fa1f72f9bca
BenzeneSnake <kelly5142200@gmail.com>
2024-05-08 23:27:59 +0800

    master test conflict

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("second commit");
             System.out.println("hot-fixed");
+            System.out.println("與hot-fixed產生衝突:master test");
         }
     }
 }
