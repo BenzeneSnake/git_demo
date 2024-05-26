4ae1d73277d67a9a46d49522d3ea7f8aff3ccc79
BenzeneSnake <kelly5142200@gmail.com>
2024-05-26 12:16:39 +0800

    test rebase test1

--- src/git_test1.java
+++ src/git_test1.java
@@ -10,5 +10,6 @@ public class git_test1 {
         System.out.print("test rebase intelliJ 2");
         System.out.print("test rebase intelliJ 3");
         System.out.print("test rebase 4");
+        System.out.print("test");
     }
 }
